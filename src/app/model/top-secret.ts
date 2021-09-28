b53c94c02b1054bafcabc0eb3080e331a25f23e0
Roman <no-subject@yandex.ru>
2021-09-28 22:02:46 +0300

    Update top-secret.ts
    top secret

--- src/app/model/top-secret.ts
+++ src/app/model/top-secret.ts
@@ -2,6 +2,7 @@
 
 export class TopSecret {
 
-    public static ApiPath:string = "http://mysite.com/api.php";
+    public static ApiPath: string = "http://mysite.com/api.php";
+    public static KppPath: string = 'http://mysite.com/kpp'; // Address to show to the unauthorized users
 
 }
